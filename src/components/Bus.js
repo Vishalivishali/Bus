e7085758ade09919b674ad36281f8029189dd69c
Vishalivishali <you@example.com>
2022-12-05 13:58:06 +0530

    Modified bus.

---
+++ src/components/Bus.js
@@ -0,0 +1,27 @@
+/* eslint-disable max-lines-per-function */
+import { React } from 'react';
+
+const Bus = () =>
+	<div>
+		<div className="roofTop"/>
+		<div className="bus"/>
+		<div className="frontGlass"/>
+		<div className="firstWindow"/>
+		<div className="secondWindow"/>
+		<div className="thirdWindow"/>
+		<div className="fourthWindow"/>
+		<div className="sticker"/>
+		<div className="road"/>
+		<div className="crossLine"/>
+		<div className="frontTyre"/>
+		<div className="frontInnerTyre"/>
+		<div className="backTyre"/>
+		<div className="backInnerTyre"/>
+		<div className="door"/>
+		<div className="rightDoor"/>
+		<div className="leftDoor"/>
+		<div className="ledRed"/>
+		<div className="ledOrange"/>
+	</div>;
+
+export default Bus;
